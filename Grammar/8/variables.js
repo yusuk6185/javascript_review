437a9c1838c8af50165b35f9c849d786ae7f1fe6
Ian Choi <yusuk6185@gmail.com>
2021-08-23 15:46:20 +0900

    first commit.

---
+++ Grammar/8/variables.js
@@ -0,0 +1,30 @@
+// VARIABLES //
+
+let value = 1;
+// 값 수정 가능
+value = 2; 
+// 두번 선언 불가능
+// let value = 2;
+
+const a = 1;
+// 수정 불가능
+// const a = 2;
+
+// var a = 1;
+// let 이랑 비슷하지만 이제 사용하지 않음. 두 번 선언 가능.
+// 구형 브라우저에서는 let, const 를 못쓰지만 -> 바벨을 통해서 사용가능 하게 변환해줌.
+
+// STRING //
+let text = 'hello';
+let name = "ian";
+
+// BOOLEAN //
+let good = true;
+let loading = false;
+
+let something = null;
+let what = undefined;
+// null -> 없다, undefined -> 아직 정해지지 않았다.
+
+
+
