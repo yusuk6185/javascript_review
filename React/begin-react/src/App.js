db21f66817c4eaab217910a556f2ef8df617d835
Ian Choi <yusuk6185@gmail.com>
2021-09-01 16:22:22 +0900

    9/01 list add, remove, update

--- React/begin-react/src/App.js
+++ React/begin-react/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 // import Hello from './Hello';
 import './App.css';
+import CreateUser from './CreateUser';
 import UserList from './UserList';
 // import Wrapper from './Wrapper';
 // import Counter from './Counter';
@@ -8,6 +9,71 @@ import UserList from './UserList';
 // import MultipleInput from './MultipleInput';
 
 function App() {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+  const { username, email } = inputs;
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  }
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'yusuk6185',
+      email: 'yusuk6185@gmail.com',
+      active: true,
+    },
+    {
+      id: 2,
+      username: 'test',
+      email: 'test@test.com',
+      active: false,
+    },
+    {
+      id: 3,
+      username: 'velopert',
+      email: 'velopert@gmail.com',
+      active: false,
+    }
+  ]);
+  // 컴포넌트 안의 변수 만들기
+  const nextId = useRef(4);
+  // 배열에 항목 추가하기
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    }
+    setUsers([...users, user]) // 불변성을 지켜줘야 함
+    // setUsers(users.concat(user)) 를 써도 같음
+    setInputs({
+      username:'',
+      email:''
+    })
+    console.log(nextId.current); // 4
+    nextId.current += 1;
+  };
+  
+  // 배열에 항목 삭제하기
+  const onRemove = id => {
+    setUsers(users.filter(user => user.id !== id));
+  }
+
+  // 배열 업데이트 하기 -> map 사용하고 spread 로 불변성 유지
+  const onToggle = id => {
+    setUsers(users.map(
+      user => user.id === id
+        ? { ...user, active: !user.active }
+        : user
+    ))
+  }
+
   return (
     <>
     {/* <Wrapper>
@@ -18,7 +84,13 @@ function App() {
     {/* <Counter/> */}
     {/* <InputSample/> */}
     {/* <MultipleInput/> */}
-    <UserList/>
+      <CreateUser 
+        username={username} 
+        email={email} 
+        onChange={onChange} 
+        onCreate={onCreate}
+      />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
     </>
   );
 }
--- React/begin-react/src/UserList.js
+++ React/begin-react/src/UserList.js
@@ -2,38 +2,39 @@ import React from 'react';
 
 // 배열 렌더링하기
 
-function User({ user }) {
+function User({ user, onRemove, onToggle }) {
+    const { username, email, id, active } = user;
     return(
         <div>
-            <b>{user.username}</b> <span>({user.email})</span>
+            <b style={{
+                color: active ? 'green' : 'black',
+                cursor: 'pointer'
+            }}
+            onClick={() => onToggle(id)}
+            >
+                {username}
+            </b>
+            &nbsp;
+            <span>({email})</span>
+            {/* parameter 를 받아서 함수를 실행하고 싶을때 -> onClick={onRemove(id)} 이렇게 하면 안됨.*/}
+            {/* 렌더링 되자마자 실행해버리기 때문에 함수를 만들어 줘야함 */}
+            <button onClick={() => onRemove(id)}>삭제</button>
         </div> 
     );   
 }
 
-function UserList() {
-    const users = [{
-        id: 1,
-        username: 'yusuk6185',
-        email: 'yusuk6185@gmail.com'
-        },
-        {
-            id: 2,
-            username: 'test',
-            email: 'test@test.com'
-        },
-        {
-            id: 3,
-            username: 'velopert',
-            email: 'velopert@gmail.com'
-        }
-    ];
-
+function UserList({ users, onRemove, onToggle }) {
     return (
         <div>
             {
                 users.map(
                     // key 가 있어야 효율적으로 업데이트 가능
-                    user => (<User user={user} key={user.id}/>)
+                    user => (<User 
+                                user={user} 
+                                key={user.id}
+                                onRemove={onRemove}
+                                onToggle={onToggle}
+                            />)
                 )
             }
         </div>
